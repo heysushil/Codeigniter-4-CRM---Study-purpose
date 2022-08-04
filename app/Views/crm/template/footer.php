f56e11928e5df957c74561e03b39a451b7f01e88
Chaudhary Sushil <10483227+heysushil@users.noreply.github.com>
2022-08-04 12:56:43 +0530

    Modifing html and css related works

---
+++ app/Views/crm/template/footer.php
@@ -0,0 +1,56 @@
+  <!-- footer start-->
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12 footer-copyright text-center">
+          <p class="mb-0">Copyright 2022 © Zeta theme by pixelstrap </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </div>
+  </div>
+  <!-- latest jquery-->
+  <script src="<?= base_url(); ?>/public/crm_assets/js/jquery-3.5.1.min.js"></script>
+  <!-- Bootstrap js-->
+  <script src="<?= base_url(); ?>/public/crm_assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+  <!-- feather icon js-->
+  <script src="<?= base_url(); ?>/public/crm_assets/js/icons/feather-icon/feather.min.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/icons/feather-icon/feather-icon.js"></script>
+  <!-- scrollbar js-->
+  <script src="<?= base_url(); ?>/public/crm_assets/js/scrollbar/simplebar.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/scrollbar/custom.js"></script>
+  <!-- Sidebar jquery-->
+  <script src="<?= base_url(); ?>/public/crm_assets/js/config.js"></script>
+  <!-- Plugins JS start-->
+  <script src="<?= base_url(); ?>/public/crm_assets/js/sidebar-menu.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/chart/knob/knob.min.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/chart/knob/knob-chart.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/chart/apex-chart/apex-chart.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/chart/apex-chart/stock-prices.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/notify/bootstrap-notify.min.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/dashboard/default.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/notify/index.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/datepicker/date-picker/datepicker.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/datepicker/date-picker/datepicker.en.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/datepicker/date-picker/datepicker.custom.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/photoswipe/photoswipe.min.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/photoswipe/photoswipe-ui-default.min.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/photoswipe/photoswipe.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/typeahead/handlebars.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/typeahead/typeahead.bundle.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/typeahead/typeahead.custom.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/typeahead-search/handlebars.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/typeahead-search/typeahead-custom.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/height-equal.js"></script>
+  <!-- Plugins JS Ends-->
+  <!-- Theme js-->
+  <script src="<?= base_url(); ?>/public/crm_assets/js/script.js"></script>
+  <script src="<?= base_url(); ?>/public/crm_assets/js/theme-customizer/customizer.js"></script>
+  <!-- login js-->
+  <!-- Plugin used-->
+</body>
+
+<!-- Mirrored from admin.pixelstrap.com/zeta/theme/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 03 Aug 2022 10:49:16 GMT -->
+
+</html>
