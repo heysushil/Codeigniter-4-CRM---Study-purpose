3e0ef65cf8e9d41cb7af88a25993e1bf6bad3310
Chaudhary Sushil <10483227+heysushil@users.noreply.github.com>
2022-08-04 11:50:37 +0530

    upload a html template

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
+        'username' => 'root',
         'password' => '',
-        'database' => '',
+        'database' => 'demo_crm_demo',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
